f961846bb02f3ed3a0e7b606afe4b081c5014d4f
floitex <mursalbekov.merey@gmail.com>
2022-02-11 11:12:28 +0600

    rgrg

--- week2/l.py
+++ week2/l.py
@@ -1,15 +1,11 @@
-s = str(input())
-cnt = 0
-cnt1 = 0
-if(len(s)%2==1):
-  print("No")
-  break
-for i in range(0, len(s)):
-  if(s[i] == "(" or s[i] == "[" or s[i] == "{"):
-    cnt = cnt + 1
-  if(s[i] == ")" or s[i] == "}" or s[i] == "]"):
-    cnt1 = cnt1 + 1
+def isBalanced(final_str):
+    type_brackets = ['()', '{}', '[]']
+    while any(x in final_str for x in type_brackets):
+        for br in type_brackets:
+            final_str = final_str.replace(br, '')
+    return not final_str
 
-if(cnt1 != cnt):
-  print("No")
-  break
+
+string = str(input())
+print("Yes"
+      if isBalanced(string) else "No")
