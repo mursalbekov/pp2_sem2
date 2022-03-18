378ec3fa6b17b0e9e08c6f6bd5ac2c858633a95c
floitex <mursalbekov.merey@gmail.com>
2022-03-18 21:37:54 +0600

    update

--- week6/builtin/5.py
+++ week6/builtin/5.py
@@ -1,12 +1,3 @@
-tuple = []
-tuple = input()
-cnt = 0
-def testtrue(tuple):
-	for i in range(0, len(tuple)):
-		if(tuple[i] == True):
-			cnt = cnt + 1
-	if (cnt == len(tuple)):
-		return True
-	else:
-		return False
-print(testtrue())
+tuple = [True, True, True, True, True]
+returnn = all(tuple)
+print(returnn)
