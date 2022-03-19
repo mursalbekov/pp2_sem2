88ffdcc424af5bee21bb27111a3c737c3ffddd13
floitex <mursalbekov.merey@gmail.com>
2022-03-19 12:58:35 +0600

    update

--- week6/dir/2.py
+++
@@ -1,8 +0,0 @@
-import os
-
-path  = os.getcwd()
-
-if os.access(path, os.F_OK): print('Exist')
-if os.access(path, os.R_OK): print('Readable')
-if os.access(path, os.W_OK): print('Writable')
-if os.access(path, os.X_OK): print('Executable')
--- week6/dir/6.py
+++
@@ -1,8 +0,0 @@
-
-
-k = 65
-
-for i in range(65, 91):
-    with open(chr(k)+'.txt', 'w') as f:
-        f.writelines('')
-    k += 1    
